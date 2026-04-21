f514bd4a4e562b140aa0cc79bbc9429d9ce16133
ryanyim-blip <ryanyim@seattleacademy.org>
2026-04-21 16:29:00 +0000

    words

---
+++ wordle.py
@@ -0,0 +1,61 @@
+import random
+
+# Pick a word at random
+word_list = ["loopy","heart","audio","laugh","trial","fishy","12345" "clamp","clunk","iiiii"]
+hidden_word = random.choice(word_list)
+
+print("WORDLE:")
+
+# Repeat for 6 guesses
+for i in range(6):
+    # Guess a word
+    guess_word = input()
+    output = ""
+
+    # First letter (in python, counting starts at 0 not 1)
+    if guess_word[0] == hidden_word[0]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+    
+    if guess_word[1] == hidden_word[1]:
+        output += "🟩"
+    elif guess_word[1] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+        
+    if guess_word[2] == hidden_word[2]:
+        output += "🟩"
+    elif guess_word[2] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    if guess_word[3] == hidden_word[3]:
+        output += "🟩"
+    elif guess_word[3] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    if guess_word[4] == hidden_word[4]:
+        output += "🟩"
+    elif guess_word[4] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+    
+    if len(guess_word) == 6:
+        output = "⬜⬜⬜⬜⬜🟥"
+        input("please type a five letter word")
+
+    # Result
+    print(output)
+    if output == "🟩🟩🟩🟩🟩":
+        print("You win")
+        break
+
+print(f"You used {i+1} guesses")
